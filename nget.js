8d232b3b5f7da55732df6097ea4f8928dbe2f8db
nanogyth <jstout.physics@gmail.com>
2022-07-06 12:55:51 -0400

    Update nget.js
    multiple files concurrently
    ns.write shouldn't be able to overlap??

--- nget.js
+++ nget.js
@@ -1,25 +1,41 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	ns.tail()
-	const name = "test.js"
-	if (ns.fileExists(name)) {
-		ns.rm(name)
-		ns.print(`removing ${name}`)
-		if (!ns.fileExists(name)) {
-			ns.print("deleted")
-		}
-	}
+	// const name = "test.js"
+	// if (ns.fileExists(name)) {
+	// 	ns.rm(name)
+	// 	ns.print(`removing ${name}`)
+	// 	if (!ns.fileExists(name)) {
+	// 		ns.print("deleted")
+	// 	}
+	// }
+
 	// await ns.wget(`https://raw.githubusercontent.com/nanogyth/bb/master/${name}`, name)
 
 	// const res = await fetch(`https://raw.githubusercontent.com/nanogyth/bb/master/${name}`)
 	// const txt = await res.text()
 	// await ns.write(name, txt, "w")
 
-	let x = fetch(`https://raw.githubusercontent.com/nanogyth/bb/master/${name}`)
-		.then(response => response.text())
-		.then(text => ns.write(name, text, "w"))
+	// let x = fetch(`https://raw.githubusercontent.com/nanogyth/bb/master/${name}`)
+	// 	.then(response => response.text())
+	// 	.then(text => ns.write(name, text, "w"))
 
 	// fetch/then train will allow script to keep running
 	// so the script will close if nothing is there to keep it open
-	await x
+	async function process_name(name) {
+		if (ns.fileExists(name)) {
+			ns.rm(name)
+			ns.print(`removing ${name}`)
+			if (!ns.fileExists(name)) {
+				ns.print("deleted")
+			}
+		}
+
+		return fetch(`https://raw.githubusercontent.com/nanogyth/bb/master/${name}`)
+			.then(response => response.text())
+			.then(text => ns.write(name, text, "w"))
+	}
+
+	ns.tail()
+	const names = ["test.js", "test2.js"]
+	await Promise.all(names.map(process_name))
 }
