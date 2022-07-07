28b16ad99b8f6e797bb531c105d0393a230e4be7
nanogyth <jstout.physics@gmail.com>
2022-07-07 13:28:28 -0400

    Update loop0.js
    Adding features (checkbox loop kill switch).
    Still problem of sec, money being stale when await term resolves.
    Next plan to async acquire the terminal, then sync recalculate.

--- loop0.js
+++ loop0.js
@@ -1,27 +1,42 @@
-import { server_data } from "data.js"
-import { aterm, delay } from "lib.js"
-import { hack_time } from "lib2.js"
+// import { server_data } from "data.js"
+import { aterm, delay, refocus, hack_time, grow_fraction, addCheckbox } from "lib.js"
 
 /** @param {NS} ns */
 export async function main(ns) {
-	const server = server_data["n00dles"]
+	let loop = addCheckbox("Loop", true);
+	ns.atExit(() => loop.remove());
+	loop.addEventListener("change", () => {
+		// ns.tprint(`just keep looping: ${loop.checked}`);
+		just_keep_looping = loop.checked;
+	});
+
+	const server = ng_server_data["n00dles"]
+	const gf = grow_fraction(server)
 	let term_busy_until = 0
-	while (true) { // global/button to stop looping?
-	// for (let i = 20; i > 0; i--) { // global/button to stop looping?
+	let msg = "weak", ht
+	while (just_keep_looping) {
+		refocus()
 		const hack_lvl = ns.getHackingLevel() // 0.05 GB
-		const above_min = ns.getServerSecurityLevel("n00dles") - 1
+		const sec = ns.getServerSecurityLevel("n00dles") - 1
 		const money = ns.getServerMoneyAvailable("n00dles") / 1_750_000
-		ns.toast(`${above_min.toFixed(3)} ${money.toFixed(3)}`)
+		ns.toast(`${sec.toFixed(3)} ${money.toFixed(3)}`)
 		// add html info, svg visual?
-		const now = performance.now()
-		if (now >= term_busy_until) {
-			let [ht, msg] = [200 * hack_time(server, hack_lvl), "c_n00dles; grow"]
-			if (above_min > 0.051 || (money == 1 && above_min > 0)) {
-				[ht, msg] = [1.25 * ht, "c_n00dles; weaken"]
+
+		if (performance.now() >= term_busy_until) {
+			// these don't update instantly???, so predict the future
+			// probably looping here and waiting before last action finished
+			// const g_threads = Math.min(25, Math.ceil(Math.log(1 / money) / Math.log(gf)))
+			// const [p_sec, p_money] = msg == "grow" ? [sec + 0.004 * g_threads, money * gf ** g_threads] : [sec - 0.05, money]
+			// const [p_sec, p_money] = [sec, money]
+			[ht, msg] = [200 * hack_time(server, hack_lvl), "grow"]
+			if (sec > 0.051 || (money >= 1 && sec > 0)) {
+				[ht, msg] = [1.25 * ht, "weaken"]
 			}
-			term_busy_until = now + ht
-			await aterm(msg)
+			ns.toast(`${sec.toFixed(3)} ${money.toFixed(3)} ${msg}`, "info", ht + 1_000)
+			// term_busy_until = now + ht // this needs to be set after the await, with a live now
+			await aterm("c_n00dles; " + msg)
+			term_busy_until = performance.now() + ht // build in extra delay?
 		}
-		await delay(Math.min(1_000, term_busy_until - now))
+		await delay(Math.min(1_000, term_busy_until - performance.now()))
 	}
 }
