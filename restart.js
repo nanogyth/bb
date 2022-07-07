5ca8094a6b5e92b02532797a73ddefb106d82818
nanogyth <jstout.physics@gmail.com>
2022-07-07 16:46:28 -0400

    Update restart.js

--- restart.js
+++ restart.js
@@ -2,11 +2,5 @@ import { restart } from "lib.js"
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    ns.rm('data.js')
-    sessionStorage.clear()
-    // for (const s of ["foodnstuff", "sigma-cosmetics", "joesguns",
-    //     "hong-fang-tea", "harakiri-sushi", "nectar-net", "n00dles", "home"]) {
-    //     ns.killall(s, true)
-    // }
-    await restart("run lib.js")
+    restart("run lib.js")
 }
