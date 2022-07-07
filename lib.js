b825cf2f4b6d3f5bcc13a8a286067063260477f4
nanogyth <jstout.physics@gmail.com>
2022-07-07 16:39:45 -0400

    large restructure
    Pack multipliers and server_data into globalThis instead of importing from file

--- lib.js
+++ lib.js
@@ -1,55 +1,116 @@
-import { multipliers } from "data.js"
-import { usi, term, write_yield_data } from "lib.js"
+// import { multipliers } from "data.js" // globalize
+// import { study, unfocus, term } from "lib.js"
+
+export let checkboxes = {},
+    doc = globalThis["document"],
+    cbDiv = doc.querySelector(".checkboxContainer")
 
 /** @param {NS} ns */
 export async function main(ns) {
-    await write_yield_data(ns, yield_server_data)
-    setTimeout(term, 0, "home; run p0.js")
+    globalThis["just_keep_looping"] = true
+    study(3)
+    cb_initialize({ ...ns.ui.getTheme(), ...ns.ui.getStyles() })
+    unfocus()
+    all_servers(ns)
+    // ns.tprint(ng_server_data)
+    setTimeout(term, 0, "home; run mult.js")
 }
 
-function* yield_server_data(ns) {
-    const server_data = { home: { route: "home" } }
+export let cb_initialize = ({ primary = "#fd0", secondary = "#28c", fontFamily = "Comic Sans MS" }) => {
+    // checkbox methods from omuretsu
+    // https://discord.com/channels/415207508303544321/924855855457910855/994342446555213916
+    let style = `<style>
+  .checkboxContainer input{
+    appearance:none;
+    font:14px "${fontFamily}";
+  }
+  .checkboxContainer input::after{
+    content:attr(data-unchecked);
+    color:${secondary};
+  }
+  .checkboxContainer input:checked::after{
+    content:attr(data-checked);
+    color:${primary};
+  }</style>`;
+    if (cbDiv) {
+        //If already initialized but isn't on page, add it back to the page if possible.  
+        if (!doc.body.contains(cbDiv)) return doc.querySelector(".react-draggable")?.appendChild(cbDiv);
+        //If already initialized and was already on the page, just change the style to what was provided
+        return cbDiv.querySelector("style").outerHTML = style;
+    }
+    cbDiv = doc.createElement("div");
+    cbDiv.classList.add("checkboxContainer");
+    cbDiv.innerHTML = style;
+    doc.querySelector(".react-draggable").appendChild(cbDiv);
+}
+
+if (!cbDiv || !doc.body.contains(cbDiv)) cb_initialize({});
+
+export let addCheckbox = (name, checked = false, uncheckedText = name + " OFF", checkedText = name + " ON") => {
+    if (checkboxes[name]) {
+        console.log(`Duplicate name ${name} was provided for a checkbox. Providing existing checkbox.`);
+        return checkboxes[name];
+    }
+    let row = cbDiv.appendChild(doc.createElement("div"));
+    row.innerHTML = `<input type=checkbox data-unchecked="${uncheckedText}" data-checked="${checkedText}"${checked ? " checked" : ""}>`;
+    let checkbox = row.querySelector("input");
+    checkbox.remove = () => {
+        row.remove();
+        delete checkboxes[name];
+    };
+    checkboxes[name] = checkbox;
+    return checkbox;
+};
+
+export function kill_dots(str) {
+    // "I.I.I.I" => "IIII"
+    // "." => ""
+    return str.replace(/\./g, "")
+}
+
+function all_servers(ns) {
+    // needs open term for alias spam
+    const server_data = {}
+    for (const [name, route] of Object.entries(routes(ns))) {
+        term(`alias c_${kill_dots(name)}="${route}"`) // alias dies on "I.I.I.I" and "."
+        server_data[name] = process_name(ns, name)
+    }
+    globalThis["ng_server_data"] = server_data
+}
+
+function process_name(ns, name) {
+    const { numOpenPortsRequired,
+        maxRam,
+        baseDifficulty,
+        minDifficulty,
+        requiredHackingSkill,
+        serverGrowth,
+        moneyMax } = ns.getServer(name)
+    return {
+        numOpenPortsRequired,
+        maxRam,
+        baseDifficulty,
+        minDifficulty,
+        requiredHackingSkill,
+        serverGrowth,
+        moneyMax
+    }
+}
+
+function routes(ns) {
+    const route_data = {}
     const found_names = ["home"]
-    const props = ["numOpenPortsRequired", "maxRam", "baseDifficulty",
-        "minDifficulty", "requiredHackingSkill", "serverGrowth", "moneyMax"]
-
-    yield "export const server_data = {\n"
-    yield* process_found_names()
-    yield "};\n\n"
-
-    function* process_found_names() {
-        for (const name of found_names) {
-            for (const new_name of ns.scan(name)) {
-                if (!found_names.includes(new_name)) {
-                    yield* process_name(new_name, server_data[name].route)
-                }
+    // can't loop over route_data directly
+    // Object.keys won't iterate over keys added later
+    for (const name of found_names) {
+        for (const new_name of ns.scan(name)) {
+            if (!route_data[new_name]) {
+                found_names.push(new_name)
+                route_data[new_name] = `${route_data[name] ?? "home"}; connect ${new_name}`
             }
         }
     }
-
-    function* process_name(name, base_route) {
-        found_names.push(name)
-        const server = ns.getServer(name)
-        if (server.purchasedByPlayer) return
-
-        yield `"${name}": {\n` // some names have dash-es, so need to be quoted
-        yield* process_props(server)
-        yield "},\n"
-
-        process_route(name, base_route)
-    }
-
-    function* process_props(server) {
-        for (const prop of props) {
-            yield `  ${prop}: ${usi(server[prop])},\n`
-        }
-    }
-
-    function process_route(name, base_route) {
-        const new_route = `${base_route}; connect ${name}`
-        term(`alias c_${name}="${new_route}"`)
-        server_data[name] = { route: new_route }
-    }
+    return route_data
 }
 
 function total_time_to_cash(server, hacking, available_threads, goal_money) {
@@ -86,8 +147,9 @@ function time_to_prep(server, hacking, available_threads) {
     return w_time + gw_time
 }
 
-export function find_best(server_data, hacking, test_func = money_per_GB_s) {
-    const best_list = Object.entries(server_data)
+// need way to pass in more args to test_func
+export function find_best(hacking, test_func = money_per_GB_s) {
+    const best_list = Object.entries(ng_server_data)
         .map(([n, s]) => [n, test_func(s, hacking)])
         .sort((x, y) => y[1] - x[1])
         .filter(([n, rate]) => rate > 0)
@@ -127,7 +189,7 @@ export function hgw_calc(server, hacking, safety = 1.1) {
 }
 
 export function hack_fraction(server, hacking, _hackDifficulty) {
-    const { hacking_money_mult, ScriptHackMoney = 1 } = multipliers
+    const { hacking_money_mult, ScriptHackMoney } = ng_multipliers
     const { requiredHackingSkill, minDifficulty, hackDifficulty } = server
     _hackDifficulty = _hackDifficulty ?? hackDifficulty ?? minDifficulty
     // _hackDifficulty ??= hackDifficulty ??= minDifficulty // does this work???
@@ -141,7 +203,7 @@ export function hack_fraction(server, hacking, _hackDifficulty) {
 }
 
 export function grow_fraction(server, cores = 1, threads = 1, _hackDifficulty = null) {
-    const { hacking_grow_mult, ServerGrowthRate = 1 } = multipliers
+    const { hacking_grow_mult, ServerGrowthRate } = ng_multipliers
     const { serverGrowth, minDifficulty, hackDifficulty } = server
     _hackDifficulty = _hackDifficulty ?? hackDifficulty ?? minDifficulty
 
@@ -156,14 +218,14 @@ export function grow_fraction(server, cores = 1, threads = 1, _hackDifficulty = 
 }
 
 export function hack_xp(server) {
-    const { hacking_exp_mult, HackExpGain = 1 } = multipliers
+    const { hacking_exp_mult, HackExpGain } = ng_multipliers
     const { baseDifficulty } = server
 
     return (3 + 0.3 * baseDifficulty * hacking_exp_mult) * HackExpGain
 }
 
 export function hack_chance(server, hacking, _hackDifficulty, int) {
-    const { hacking_chance_mult } = multipliers
+    const { hacking_chance_mult } = ng_multipliers
     const { requiredHackingSkill, minDifficulty, hackDifficulty } = server
     _hackDifficulty = _hackDifficulty ?? hackDifficulty ?? minDifficulty
 
@@ -177,7 +239,7 @@ export function hack_chance(server, hacking, _hackDifficulty, int) {
 }
 
 export function hack_time(server, hacking, _hackDifficulty, int) {
-    const { hacking_speed_mult } = multipliers
+    const { hacking_speed_mult } = ng_multipliers
     const { requiredHackingSkill, minDifficulty, hackDifficulty } = server
     _hackDifficulty = _hackDifficulty ?? hackDifficulty ?? minDifficulty
 
@@ -258,4 +320,142 @@ export function hgw_split(ns, threads, target, hack_fraction, grow_fraction, mon
     w = Math.ceil(w)
     h = Math.floor((threads - gm * g - wm * w) / hm)
     ns.tprintf(`${target} ${hm}*${h} +${gm}*${g} +${wm}*${w} = ${hm * h + gm * g + wm * w}`)
+}
+
+export const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+// function underscore_separated_integer(n, separator = '_') {
+export function usi(n, separator = '_') {
+    let s = Math.round(n).toString()
+    if (s.length > 3) {
+        s = s.replace(/\B(?=(?:\d{3})+(?!\d))/g, separator);
+    }
+    return s
+}
+
+// const doc = eval("document")
+
+export function unfocus() {
+    return query_click("button", "Do something else simultaneously")
+}
+
+export function refocus() {
+    return query_click("tr th button", "Focus")
+}
+
+function get_to_page(page, func) {
+    const did_unfocus = unfocus()
+    query_click("div[role='button'] div.MuiListItemText-root p", page)
+    if (func) {
+        func()
+        click_all_aria_hidden()
+        if (did_unfocus) refocus()
+    }
+    return did_unfocus
+}
+
+export function term(message) {
+    get_to_page("Terminal")
+    const terminal = doc.querySelector("#terminal-input");
+    const event_key = Object.keys(terminal)[1];
+    terminal[event_key].onChange({ target: { value: message } });
+    terminal[event_key].onKeyDown({ key: 'Enter', preventDefault: () => null });
+}
+
+export async function aterm(message) {
+    const [terminal, did_unfocus] = await get_term()
+    const event_key = Object.keys(terminal)[1];
+    terminal[event_key].onChange({ target: { value: message } });
+    terminal[event_key].onKeyDown({ key: 'Enter', preventDefault: () => null });
+    if (did_unfocus) refocus()
+}
+
+export async function get_term() {
+    let did_unfocus, terminal
+    while (true) {
+        did_unfocus = get_to_page("Terminal") || did_unfocus
+        terminal = doc.querySelector("#terminal-input")
+        if (!terminal.matches(":disabled")) {
+            return [terminal, did_unfocus]
+        }
+        await delay(16) // because of this await you might not stay on the right page
+    }
+}
+
+// what if already joined??
+export async function join_fac(target, work = "Hacking Contracts") {
+    await join(target)
+    for (const el of doc.querySelectorAll("div span h6 span")) {
+        if (el.innerHTML == target) {
+            el.parentNode.parentNode.previousSibling.firstChild.click()
+        }
+    }
+    query_click("button", work)
+}
+
+async function join(target, loop_time = 1_000) {
+    while (true) {
+        get_to_page("Factions")
+        for (const el of doc.querySelectorAll("div span h6 span")) {
+            if (el.innerHTML == target) {
+                sudo_click(el.parentNode.parentNode.previousSibling)
+                return
+            }
+        }
+        await delay(loop_time)
+    }
+}
+
+function query_click(query, text_match) {
+    for (let el of doc.querySelectorAll(query)) {
+        if (el.innerHTML.startsWith(text_match)) { // innerText, textContent ??
+            el.click()
+            return true
+        }
+    }
+    return false // not found, unable to click
+}
+
+function sudo_click(element) {
+    element[Object.keys(element)[1]].onClick({ isTrusted: true })
+}
+
+function click_all_aria_hidden() {
+    for (const el of doc.querySelectorAll("div[aria-hidden]")) {
+        el.click()
+    }
+}
+
+export function restart(msg) {
+    get_to_page("Options")
+    doc.querySelector("button[aria-label='Perform a soft reset. Resets everything as if you had just purchased an Augmentation.']").click()
+    setTimeout(term, 0, msg)
+}
+
+export function study(lvl = 3) {
+    // lvl cost/s xp/s
+    //   0      0    1
+    //   1    120    2
+    //   2    240    4
+    //   3    960    8
+    get_to_page("City")
+    doc.querySelector("span[aria-label='Rothman University']").click()
+    doc.querySelectorAll("button[aria-label='Gain hacking experience!']")[lvl].click()
+}
+
+export function travel(target = "C") {
+    get_to_page("Travel", () => {
+        query_click("div h4~div p span", target)
+    })
+}
+
+export function get_tor() {
+    get_to_page("City", () => {
+        doc.querySelector("span[aria-label='Alpha Enterprises'],\
+        span[aria-label='ECorp'],\
+        span[aria-label='CompuTek'],\
+        span[aria-label='Omega Software']").click()
+
+        query_click("button", "Purchase TOR")
+    })
 }
