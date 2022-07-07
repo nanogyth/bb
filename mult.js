b72a1bb3ce54cd4f99c0d19ab41963cb65252828
nanogyth <jstout.physics@gmail.com>
2022-07-07 16:43:20 -0400

    just set_multipliers
    everything else moved to lib

--- mult.js
+++ mult.js
@@ -1,36 +1,17 @@
-// no imports
+import { term } from "lib.js"
 
 /** @param {NS} ns */
 export async function main(ns) {
-	await clear_data(ns)
-	await write_yield_data(ns, yield_multipliers)
-	setTimeout(term, 0, "home; run lib2.js")
+	set_multipliers(ns)
+	// ns.tprint(ng_multipliers)
+	setTimeout(term, 0, "home; run p0.js")
 }
 
-async function clear_data(ns, target = "data.js") {
-	sessionStorage.clear()
-	await ns.write(target, "\n", "w")
-}
-
-export async function write_yield_data(ns, gen, target = "data.js") {
-	for (const data of gen(ns)) {
-		await ns.write(target, data)
-	}
-}
-
-function* yield_multipliers(ns) {
-	yield "export const multipliers = {\n"
-	yield* Object.entries(all_constants(ns)).map(([k, v]) => ` ${k}: ${v},\n`)
-	yield "};\n\n"
-}
-
-function all_constants(ns, player, owned_source_files) {
+function set_multipliers(ns, player, owned_source_files) {
 	const pc = player_constants(player ?? ns.getPlayer())
 	pc.sfs = source_files(owned_source_files ?? ns.getOwnedSourceFiles())
-	// pc.sfs = source_files([{ n: 1, lvl: 1 }, { n: 5, lvl: 2 }, { n: 12, lvl: 8 }])
 	const bnc = bit_node_constants(pc)
-	pc.sfs = JSON.stringify(pc.sfs)
-	return Object.assign(pc, bnc)
+	globalThis["ng_multipliers"] = Object.assign(pc, bnc)
 }
 
 function player_constants(player) {
@@ -43,7 +24,7 @@ function source_files(owned_source_files) {
 }
 
 function bit_node_constants(pc, bit_node = pc.bitNodeN, sf12 = pc.sfs['12']) {
-	// Math.pow = 1 when exponent is null, but NaN if undefined =_=
+	// 1.02 ** null == 1; 1.02 ** undefined == NaN
 	const inc = 1.02 ** (sf12 ?? 0)
 	const dec = 1 / inc
 
@@ -78,133 +59,4 @@ function bit_node_constants(pc, bit_node = pc.bitNodeN, sf12 = pc.sfs['12']) {
 		{ PurchasedServerSoftcap: 1.6, HackingLevelMultiplier: 0.25, StrengthLevelMultiplier: 0.7, DefenseLevelMultiplier: 0.7, DexterityLevelMultiplier: 0.7, AgilityLevelMultiplier: 0.7, ServerMaxMoney: 0.45, ServerStartingMoney: 0.75, ServerStartingSecurity: 3, ScriptHackMoney: 0.2, CompanyWorkMoney: 0.4, CrimeMoney: 0.4, HacknetNodeMoney: 0.4, CodingContractMoney: 0.4, CompanyWorkExpGain: 0.5, ClassGymExpGain: 0.5, FactionWorkExpGain: 0.5, HackExpGain: 0.1, CrimeExpGain: 0.5, FactionWorkRepGain: 0.6, FourSigmaMarketDataCost: 10, FourSigmaMarketDataApiCost: 10, CorporationValuation: 0.001, BladeburnerRank: 0.45, BladeburnerSkillCost: 2, StaneksGiftPowerMultiplier: 2, StaneksGiftExtraSize: 1, GangSoftcap: 0.3, CorporationSoftCap: 0.3, WorldDaemonDifficulty: 3 },
 	]
 	return Object.assign(bnm[0], bnm[bit_node])
-}
-
-export const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-// function underscore_separated_integer(n, separator = '_') {
-export function usi(n, separator = '_') {
-	let s = Math.round(n).toString()
-	if (s.length > 3) {
-		s = s.replace(/\B(?=(?:\d{3})+(?!\d))/g, separator);
-	}
-	return s
-}
-
-const doc = eval("document")
-
-function get_to_page(page) {
-	const did_unfocus = query_click("button", "Do something else simultaneously")
-	query_click("div[role='button'] div.MuiListItemText-root p", page)
-	return did_unfocus
-}
-
-async function refocusing_get_to_page(page, func) {
-	const did_unfocus = query_click("button", "Do something else simultaneously")
-	query_click("div[role='button'] div.MuiListItemText-root p", page)
-	if (func) {
-		await func()
-		click_all_aria_hidden()
-		if (did_unfocus) refocus()
-	}
-	return did_unfocus
-}
-
-export function term(message) {
-	const terminal = doc.querySelector("#terminal-input");
-	const event_key = Object.keys(terminal)[1];
-	terminal[event_key].onChange({ target: { value: message } });
-	terminal[event_key].onKeyDown({ key: 'Enter', preventDefault: () => null });
-}
-
-export async function aterm(message, wait = -1) {
-	await refocusing_get_to_page("Terminal", async () => {
-		const terminal = doc.querySelector("#terminal-input");
-		const event_key = Object.keys(terminal)[1];
-		while (terminal.matches(":disabled")) await delay(16)
-		terminal[event_key].onChange({ target: { value: message } });
-		terminal[event_key].onKeyDown({ key: 'Enter', preventDefault: () => null });
-	})
-	if (wait >= 0) await delay(wait)
-}
-
-function query_click(query, text_match) {
-	for (let el of doc.querySelectorAll(query)) {
-		if (el.innerHTML.startsWith(text_match)) { // innerText, textContent ??
-			el.click()
-			return true
-		}
-	}
-	return false // not found, unable to click
-}
-
-function sudo_click(element) {
-	element[Object.keys(element)[1]].onClick({ isTrusted: true })
-}
-
-export async function join_fac(target, work = "Hacking Contracts") {
-	get_to_page("Factions")
-	await join(target)
-	for (const el of doc.querySelectorAll("div span h6 span")) {
-		if (el.innerHTML == target) {
-			el.parentNode.parentNode.previousSibling.firstChild.click()
-		}
-	}
-	query_click("button", work)
-}
-
-async function join(target, loop_time = 1_000) {
-	while (true) {
-		for (const el of doc.querySelectorAll("div span h6 span")) {
-			if (el.innerHTML == target) {
-				sudo_click(el.parentNode.parentNode.previousSibling)
-				return
-			}
-		}
-		await delay(loop_time)
-	}
-}
-
-function refocus() {
-	return query_click("tr th button", "Focus")
-}
-
-function click_all_aria_hidden() {
-	for (const el of doc.querySelectorAll("div[aria-hidden]")) {
-		el.click()
-	}
-}
-
-export function restart(msg) {
-	get_to_page("Options")
-	doc.querySelector("button[aria-label='Perform a soft reset. Resets everything as if you had just purchased an Augmentation.']").click()
-	setTimeout(term, 0, msg)
-}
-
-export function study(lvl = 3) {
-	// lvl cost/s xp/s
-	//   0      0    1
-	//   1    120    2
-	//   2    240    4
-	//   3    960    8
-	get_to_page("City")
-	doc.querySelector("span[aria-label='Rothman University']").click()
-	doc.querySelectorAll("button[aria-label='Gain hacking experience!']")[lvl].click()
-}
-
-export async function travel(target = "C") {
-	await refocusing_get_to_page("Travel", () => {
-		query_click("div h4~div p span", target)
-	})
-}
-
-export async function get_tor() {
-	await refocusing_get_to_page("City", () => {
-		doc.querySelector("span[aria-label='Alpha Enterprises'],\
-        span[aria-label='ECorp'],\
-        span[aria-label='CompuTek'],\
-        span[aria-label='Omega Software']").click()
-
-		query_click("button", "Purchase TOR")
-	})
 }
