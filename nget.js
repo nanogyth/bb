977f1542c8fc8ec387cfe07dfc0160e281e2780f
nanogyth <jstout.physics@gmail.com>
2022-07-09 17:25:54 -0400

    h.js -> act.js

--- nget.js
+++ nget.js
@@ -1,12 +1,11 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	async function process_name(name) {
-		return fetch(`https://raw.githubusercontent.com/nanogyth/bb/master/${name}`)
-			.then(response => response.text())
-			.then(text => ns.write(name, text, "w"))
-	}
-	
-	await Promise.all(["lib.js", "mult.js", "p0.js", "h.js", "loop0.js", "restart.js"].map(process_name))
-	
-    ns.run("lib.js")
+    async function process_name(name) {
+        return fetch(`https://raw.githubusercontent.com/nanogyth/bb/master/${name}`)
+            .then(response => response.text())
+            .then(text => ns.write(name, text, "w"))
+    }
+    
+    await Promise.all(["lib.js", "mult.js", "p0.js", "act.js", "loop0.js", "restart.js"].map(process_name))
+    ns.tprint("setup options then run restart.js")
 }
