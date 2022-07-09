452bccde8f1b67aa883c731fd94851c3b7e5b40e
nanogyth <jstout.physics@gmail.com>
2022-07-09 19:17:17 -0400

    Update restart.js

--- restart.js
+++ restart.js
@@ -2,5 +2,7 @@ import { restart } from "lib.js"
 
 /** @param {NS} ns **/
 export async function main(ns) {
+    globalThis["now_offset"] = performance.now()
+    ns.write("log.txt", `${new Date().toString()}`, "w")
     restart("run lib.js")
 }
